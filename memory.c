83339f140884efe4bf922cd237ba210a7f39f0d9
ProgramHenry <jsh9998@nyu.edu>
2023-12-10 11:31:37 -0600

    test
    test

--- memory.c
+++ memory.c
@@ -23,35 +23,44 @@ struct MEMORY_BLOCK best_fit_allocate(int request_size, struct MEMORY_BLOCK memo
     struct MEMORY_BLOCK NULLBLOCK;
     struct MEMORY_BLOCK newBlock;
 
+    // Find best fit block
+    int blockIndex = -1;
     for (int x = 0; x < *map_cnt; x++)
     {
-        if ((request_size == memory_map[x].segment_size) && (memory_map[x].process_id == 0))
+        if (memory_map[x].segment_size >= request_size && memory_map[x].process_id == 0)
         {
-            memory_map[x].process_id = process_id;
-            return memory_map[x];
-        }
-        else if ((request_size < memory_map[x].segment_size) && (memory_map[x].process_id == 0))
-        {
-
-            newBlock.process_id = 0;
-            newBlock.end_address = memory_map[x].end_address;
-            memory_map[x].end_address = memory_map[x].start_address + request_size - 1;
-            memory_map[x].process_id = process_id;
-            memory_map[x].segment_size = request_size;
-
-            int excessMemory = memory_map[x].segment_size - request_size;
-            newBlock.segment_size = excessMemory;
-            newBlock.start_address = memory_map[x].end_address + 1;
-
-            memory_map[*map_cnt] = newBlock;
-            (*map_cnt)++;
-
-            return memory_map[x];
+            if (blockIndex == -1 || memory_map[x].segment_size < memory_map[blockIndex].segment_size)
+            {
+                blockIndex = x;
+            }
         }
     }
 
-    NULLBLOCK = nullBlockCreation();
-    return NULLBLOCK;
+    // Return nullblock if no available fit found
+    if (blockIndex == -1)
+    {
+        NULLBLOCK = nullBlockCreation();
+        return NULLBLOCK;
+    }
+
+    // Split the block if needed
+    if (memory_map[blockIndex].segment_size > request_size)
+    {
+        newBlock.process_id = 0;
+        newBlock.end_address = memory_map[blockIndex].end_address;
+        memory_map[blockIndex].end_address = memory_map[blockIndex].start_address + request_size - 1;
+        memory_map[blockIndex].process_id = process_id;
+        memory_map[blockIndex].segment_size = request_size;
+
+        int excessMemory = memory_map[blockIndex].segment_size - request_size;
+        newBlock.segment_size = excessMemory;
+        newBlock.start_address = memory_map[blockIndex].end_address + 1;
+
+        memory_map[*map_cnt] = newBlock;
+        (*map_cnt)++;
+    }
+
+    return memory_map[blockIndex];
 };
 struct MEMORY_BLOCK first_fit_allocate(int request_size, struct MEMORY_BLOCK memory_map[MAPMAX], int *map_cnt, int process_id){
 
